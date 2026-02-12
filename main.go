8722ed8e1f0f74a6066ff39ad318dfac750eac94
Yannick Peter <yannick.peter92@gmail.com>
2026-02-12 16:47:27 +0100

    rewrite to text layout

--- main.go
+++ main.go
@@ -1,74 +1,75 @@
 package main
 
-import (
-	"fmt"
-	"log"
-	"os"
-	"time"
+// A simple program demonstrating the text input component from the Bubbles
+// component library.
 
+import (
+	"log"
+	"strings"
+
+	"github.com/charmbracelet/bubbles/textinput"
 	tea "github.com/charmbracelet/bubbletea"
+	"github.com/funkymcb/fremorizer/render"
 )
 
 func main() {
-	// Log to a file. Useful in debugging since you can't really log to stdout.
-	logfilePath := os.Getenv("BUBBLETEA_LOG")
-	if logfilePath != "" {
-		if _, err := tea.LogToFile(logfilePath, "fremorizer"); err != nil {
-			log.Fatal(err)
-		}
-	}
-
-	p := tea.NewProgram(model(5))
+	p := tea.NewProgram(initialModel())
 	if _, err := p.Run(); err != nil {
 		log.Fatal(err)
 	}
 }
 
-// A model can be more or less any type of data. It holds all the data for a
-// program, so often it's a struct. For this simple example, however, all
-// we'll need is a simple integer.
-type model int
+type (
+	errMsg error
+)
 
-// Init optionally returns an initial command we should run. In this case we
-// want to start the timer.
-func (m model) Init() tea.Cmd {
-	return tick
+type model struct {
+	textInput textinput.Model
+	err       error
 }
 
-// Update is called when messages are received. The idea is that you inspect the
-// message and send back an updated model accordingly. You can also return
-// a command, which is a function that performs I/O and returns a message.
+func initialModel() model {
+	ti := textinput.New()
+	ti.Placeholder = "E"
+	ti.Focus()
+	ti.CharLimit = 5
+	ti.Width = 20
+
+	return model{
+		textInput: ti,
+		err:       nil,
+	}
+}
+
+func (m model) Init() tea.Cmd {
+	return textinput.Blink
+}
+
 func (m model) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
+	var cmd tea.Cmd
+
 	switch msg := msg.(type) {
 	case tea.KeyMsg:
-		switch msg.String() {
-		case "ctrl+c", "q":
+		switch msg.Type {
+		case tea.KeyEnter, tea.KeyCtrlC, tea.KeyEsc:
 			return m, tea.Quit
-		case "ctrl+z":
-			return m, tea.Suspend
 		}
 
-	case tickMsg:
-		m--
-		if m <= 0 {
-			return m, tea.Quit
-		}
-		return m, tick
+	// We handle errors just like any other message
+	case errMsg:
+		m.err = msg
+		return m, nil
 	}
-	return m, nil
+
+	m.textInput, cmd = m.textInput.Update(msg)
+	return m, cmd
 }
 
-// View returns a string based on data in the model. That string which will be
-// rendered to the terminal.
 func (m model) View() string {
-	return fmt.Sprintf("Hi. This program will exit in %d seconds.\n\nTo quit sooner press ctrl-c, or press ctrl-z to suspend...\n", m)
-}
+	view := strings.Builder{}
+	view.WriteString(render.Fretboard())
+	view.WriteString(m.textInput.View())
+	view.WriteString("\n(esc to quit)\n")
 
-// Messages are events that we respond to in our Update function. This
-// particular one indicates that the timer has ticked.
-type tickMsg time.Time
-
-func tick() tea.Msg {
-	time.Sleep(time.Second)
-	return tickMsg{}
+	return view.String()
 }

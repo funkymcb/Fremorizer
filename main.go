27c3855b698163cd6404203f11d8d7228d8f65f0
Yannick Peter <yannick.peter92@gmail.com>
2026-02-23 17:05:33 +0100

    implement single note game logic

--- main.go
+++ main.go
@@ -8,8 +8,8 @@ import (
 
 	"github.com/charmbracelet/bubbles/textinput"
 	tea "github.com/charmbracelet/bubbletea"
+	"github.com/funkymcb/fremorizer/game"
 	"github.com/funkymcb/fremorizer/instrument"
-	"github.com/funkymcb/fremorizer/mode"
 )
 
 func main() {
@@ -21,41 +21,50 @@ func main() {
 
 type tickMsg time.Time
 
+type gameMsg struct{ msg string }
+
 type errMsg struct{ err error }
 
 func (e errMsg) Error() string { return e.err.Error() }
 
 type model struct {
 	// gameModes     []string // TODO: add different game modes
-	textInput textinput.Model
-	guitar    *instrument.Guitar // TODO: model should contain instrument, not a guitar
 	blink     int
 	err       error
+	game      game.Game
+	gameMsg   string
+	guitar    *instrument.Guitar // TODO: model should contain instrument, not a guitar
+	textInput textinput.Model
 }
 
 func initialModel() model {
 	ti := textinput.New()
-	ti.Placeholder = "E"
+	ti.Placeholder = "_"
 	ti.Focus()
 	ti.CharLimit = 5
 	ti.Width = 20
 
 	// TODO: ask the user for tuning and frets
 	g, err := instrument.NewGuitar([]string{"E", "A", "D", "G", "B", "E"}, 24)
+	game, _ := game.New("single", g)
 	if err != nil {
 		return model{
-			textInput: ti,
-			guitar:    g,
 			blink:     0,
 			err:       err,
+			game:      game,
+			gameMsg:   "Find the note",
+			guitar:    g,
+			textInput: ti,
 		}
 	}
 
 	return model{
-		textInput: ti,
-		guitar:    g,
 		blink:     0,
 		err:       nil,
+		game:      game,
+		gameMsg:   "Find the note",
+		guitar:    g,
+		textInput: ti,
 	}
 }
 
@@ -66,9 +75,15 @@ func tick() tea.Cmd {
 }
 
 func (m model) Init() tea.Cmd {
-	mode.RandomSingleNoteToBeDetermined(m.guitar) // TODO: init based on game mode
-	return tick()
+	return tea.Batch(
+		tick(),
+		textinput.Blink,
+	)
 }
+
+// FIX: game stops after a few rounds,
+// likely due to a bug in the game logic where the note is not being reset properly after each round.
+// Need to investigate and fix the issue to ensure the game continues indefinitely as intended.
 
 func (m model) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
 	var cmd tea.Cmd
@@ -78,12 +93,30 @@ func (m model) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
 		switch msg.Type {
 		case tea.KeyCtrlC, tea.KeyEsc:
 			return m, tea.Quit
+		case tea.KeyEnter:
+			input := m.textInput.Value()
+			if m.game.CheckAnswer(input) {
+				return m, func() tea.Msg {
+					game.Next(m.game)
+					return gameMsg{msg: fmt.Sprintf("Correct! The answer was '%s'.", input)}
+				}
+			} else {
+				// return error message if answer is incorrect
+				return m, func() tea.Msg {
+					return gameMsg{msg: fmt.Sprintf("Incorrect. The answer was not '%s'. Try again!", input)}
+				}
+			}
 		}
 
 		switch msg.String() {
 		case "q":
 			return m, tea.Quit
 		}
+
+	case gameMsg:
+		m.textInput.SetValue("")
+		m.gameMsg = msg.msg
+		return m, nil
 
 	case tickMsg:
 		// Toggle between 0 and 1
@@ -112,6 +145,7 @@ func (m model) View() string {
 	view := strings.Builder{}
 	view.WriteString(instrument.Render(m.guitar, m.blink))
 	view.WriteString(m.textInput.View())
+	view.WriteString("\n" + m.gameMsg + "\n")
 	view.WriteString("\n(esc to quit)\n")
 
 	return view.String()
